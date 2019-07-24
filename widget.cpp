46da24effa7fb79ac4b3e285f16b3d809655c6e4
isak-yun-bob <52562063+isak-yun-bob@users.noreply.github.com>
2019-07-24 21:42:49 +0900

    Upload vending_machine files 190724
    Upload vending_machine files 190724

---
+++ widget.cpp
@@ -0,0 +1,132 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QMessageBox>
+#include <string>
+using namespace std;
+
+
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    ui->pbCoffee->setEnabled(false);
+    ui->pbCola->setEnabled(false);
+    ui->pbTea->setEnabled(false);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+// Member function
+// Write 'Widget::' in front
+
+void Widget::changeMoney(int n) {
+    Widget::money += n;
+    ui->lcdNumber->display(money);
+
+    if (Widget::money < 100) {
+        ui->pbCoffee->setEnabled(false);
+        ui->pbCola->setEnabled(false);
+        ui->pbTea->setEnabled(false);
+    }
+    else if (100 <= Widget::money && Widget::money < 150) {
+        ui->pbCoffee->setEnabled(true);
+        ui->pbCola->setEnabled(false);
+        ui->pbTea->setEnabled(false);
+    }
+    else if (150 <= Widget::money && Widget::money < 200) {
+        ui->pbCoffee->setEnabled(true);
+        ui->pbCola->setEnabled(true);
+        ui->pbTea->setEnabled(false);
+    }
+    else {
+        ui->pbCoffee->setEnabled(true);
+        ui->pbCola->setEnabled(true);
+        ui->pbTea->setEnabled(true);
+    }
+}
+
+
+void Widget::on_pb_10_clicked()
+{
+    changeMoney(10);
+    // QMessageBox msg;
+    // msg.information(nullptr, "title", "text");
+
+}
+
+void Widget::on_pb_50_clicked()
+{
+    changeMoney(50);
+}
+
+void Widget::on_pb_100_clicked()
+{
+    changeMoney(100);
+}
+
+void Widget::on_pb_500_clicked()
+{
+    changeMoney(500);
+}
+
+void Widget::on_pbCoffee_clicked()
+{
+    changeMoney(-100);
+}
+
+void Widget::on_pbTea_clicked()
+{
+    changeMoney(-150);
+}
+
+void Widget::on_pbCola_clicked()
+{
+    changeMoney(-200);
+}
+
+void Widget::on_pbReset_clicked()
+{
+    // change : 10, 50, 100, 500 won
+    int ten = 0, fifty = 0, hund = 0, fiveHund = 0;
+    fiveHund = (Widget::money / 500);
+    Widget::money %= 500;
+
+    hund = (Widget::money / 100);
+    Widget::money %= 100;
+
+    fifty = (Widget::money / 50);
+    Widget::money %= 50;
+
+    ten = (Widget::money / 10);
+
+    /*
+    QMessageBox msg;
+    msg.setWindowTitle("Change");
+    QString msgDetail = "500won : " + to_string(fiveHund) + \
+            "\n100won : " + to_string(hund) + \
+            "\n50won : " + to_string(fifty) + \
+            "\n10won : " + to_string(ten);
+    //msg.information(nullptr, "Change", msgDetail);
+    QString msgDetail = "Hello";
+    msg.setText(msgDetail);
+    */
+    QMessageBox msg;
+
+    // Set message and use integer with QVariant().tostring()
+    QString msgDetail = "500won : " + QVariant(fiveHund).toString() + \
+            "\n100won : " + QVariant(hund).toString() + \
+            "\n50won : " + QVariant(fifty).toString() + \
+            "\n10won : " + QVariant(ten).toString();
+
+    // QString msgDetail = QStringLiteral("500won : %d\n100won : %d\n").arg(fiveHund, hund);
+    msg.information(nullptr, "Change", msgDetail);
+    Widget::money = 0;
+    ui->lcdNumber->display(money);
+
+
+}
